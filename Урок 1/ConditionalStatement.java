d68785eec9b0fb5e6ad910e027bbfa7a7ec177f4
zviktor200 <72513856+zviktor200@users.noreply.github.com>
2020-10-09 22:37:26 +0300

    Update ConditionalStatement.java

--- Урок 1/ConditionalStatement.java
+++ Урок 1/ConditionalStatement.java
@@ -1,16 +1,16 @@
 public class ConditionalStatement {
 	public static void main(String[] args){
 		int year = 21;
-		int male = 1;
+		boolean isMale = true;
 		int height = 179;
 		char firstLetterOfName = 'I';
 		if(year>20){
 			System.out.println("yes");	
 		}
-		if(male == 1){
+		if(isMale){
 			System.out.println("yes");
 		}
-		if(male != 1){
+		if(!isMale){
 			System.out.println("no");
 		}
 		if(height<180){
@@ -23,6 +23,6 @@ public class ConditionalStatement {
 } else if(firstLetterOfName == 'I') {
     System.out.println("no");
 } else {
-    System.out.println("UPS");}
+    System.out.println("UPS"); }
 }
 }
