51ef534d6c15b4a68dba1a40af41adac8adb8578
zviktor200 <72513856+zviktor200@users.noreply.github.com>
2020-10-09 22:38:13 +0300

    Update Variable.java

--- Урок 1/Variable.java
+++ Урок 1/Variable.java
@@ -1,28 +1,21 @@
-public class ConditionalStatement {
-	public static void main(String[] args){
-		int year = 21;
-		boolean isMale = true;
-		int height = 179;
-		char firstLetterOfName = 'I';
-		if(year>20){
-			System.out.println("yes");	
-		}
-		if(isMale){
-			System.out.println("yes");
-		}
-		if(!isMale){
-			System.out.println("no");
-		}
-		if(height<180){
-			System.out.println("yes");
-		} else {
-			System.out.println("no");
-		}
-		if(firstLetterOfName == 'M') {
-    System.out.println("yes");
-} else if(firstLetterOfName == 'I') {
-    System.out.println("no");
-} else {
-    System.out.println("UPS"); }
+public class Variable {
+	public static void main(String[] args) {
+		char cpu = '3';
+		byte ram = 4;
+		int hdd = 500;
+		float frequency = 2.30f;
+		double core = 2;
+		short typeSystem = 64;
+		long versionWin = 10;
+		boolean laptop = true;
+
+		System.out.println("Процессор: i" + cpu);
+		System.out.println("ОЗУ:" + ram + "гб");
+		System.out.println("Частота:" + frequency + "Gz");
+		System.out.println("Кол-во ядер:" + core);
+		System.out.println("Жесткий:" + hdd + "гб");
+		System.out.println("Тип системы" + typeSystem);
+		System.out.println("Версия Windows-" + versionWin);
+		System.out.println("Laptop " + laptop);
 }
 }
